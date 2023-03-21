9e82f7d6b2706e99fa2aea3b718ac007f266d626
Sanjay <117789437+errorsandbug@users.noreply.github.com>
2023-03-21 12:55:35 +0530

    Add files via upload

---
+++ QR code generator/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,request
+import qrcode
+
+app = Flask(__name__)
+app.secret_key="Admin_123"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
